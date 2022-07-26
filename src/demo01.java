90ff641cc48f595184dc613523a91bfd5665a0b0
Xia.Di <77670706+GitHub-XiaDi@users.noreply.github.com>
2022-07-26 11:52:04 +0800

    github create git-test

--- src/demo01.java
+++ src/demo01.java
@@ -3,6 +3,7 @@ public class demo01 {
     public static void main(String[] args) {
         System.out.println("1st");
         System.out.println("2nd");
+        System.out.println("git hub for first");
     }
 
 }
